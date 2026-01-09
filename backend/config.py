35a9ac2aa2eb997f4431abc00c02a8a7d483e59f
izakhiddinov <izakhiddinov@gmail.com>
2026-01-10 02:02:36 +0500

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,9 +1,13 @@
-# Конфигурация проекта
-BROKER_IP = "localhost"
-BROKER_PORT = 1883
+# Общая конфигурация проекта
 
-DB_HOST = "localhost"
-DB_PORT = 5432
-DB_USER = "hydrofarm"
-DB_PASS = "password"
-DB_NAME = "hydrofarm_db"
+MQTT_BROKER_HOST = "localhost"
+MQTT_BROKER_PORT = 1883
+
+POSTGRES_HOST = "localhost"
+POSTGRES_PORT = 5432
+POSTGRES_DB = "hydrofarm"
+POSTGRES_USER = "hydrofarm"
+POSTGRES_PASSWORD = "hydrofarm"
+
+MQTT_TOPIC_SENSORS = "hydrofarm/sensors/#"
+MQTT_TOPIC_COMMANDS = "hydrofarm/commands/#"
