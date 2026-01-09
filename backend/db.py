bc1919af48309ad00a538a20e4c8ce2d283b7ac7
izakhiddinov <izakhiddinov@gmail.com>
2026-01-10 02:37:57 +0500

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,11 +1,10 @@
 import psycopg2
-from config import *
+from config import POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD
 
 def get_connection():
     return psycopg2.connect(
         host=POSTGRES_HOST,
-        port=POSTGRES_PORT,
-        dbname=POSTGRES_DB,
+        database=POSTGRES_DB,
         user=POSTGRES_USER,
         password=POSTGRES_PASSWORD
     )
