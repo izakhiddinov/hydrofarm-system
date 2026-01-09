917e809992e18c79c1c527dc6678ba8307e0e4e2
izakhiddinov <izakhiddinov@gmail.com>
2026-01-10 02:59:48 +0500

    Create config.h

---
+++ esp32/esp32_module_01/config.h
@@ -0,0 +1,9 @@
+// Конфигурация ESP32 Module 01
+// Будет заполнено позже
+
+#define DEVICE_ID "esp32_module_01"
+
+// Датчики (включение / отключение)
+#define HAS_TEMP_HUMIDITY_SENSOR true
+#define HAS_CO2_SENSOR true
+#define HAS_LIGHT_SENSOR true
