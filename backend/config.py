679874aed5a33eb0d133c44e209cb62536d9bd2b
izakhiddinov <izakhiddinov@gmail.com>
2026-01-10 02:36:03 +0500

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,13 +1,16 @@
-# Общая конфигурация проекта
+import os
 
-MQTT_BROKER_HOST = "localhost"
-MQTT_BROKER_PORT = 1883
-
-POSTGRES_HOST = "localhost"
-POSTGRES_PORT = 5432
-POSTGRES_DB = "hydrofarm"
-POSTGRES_USER = "hydrofarm"
-POSTGRES_PASSWORD = "hydrofarm"
-
+# MQTT
+MQTT_BROKER = os.getenv("MQTT_BROKER", "localhost")
+MQTT_PORT = int(os.getenv("MQTT_PORT", 1883))
 MQTT_TOPIC_SENSORS = "hydrofarm/sensors/#"
 MQTT_TOPIC_COMMANDS = "hydrofarm/commands/#"
+
+# PostgreSQL
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "hydrofarm")
+POSTGRES_USER = os.getenv("POSTGRES_USER", "hydrofarm_user")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "hydrofarm_pass")
+
+# System
+DEBUG = True
